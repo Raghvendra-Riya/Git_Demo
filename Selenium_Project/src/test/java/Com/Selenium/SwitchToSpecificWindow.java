663fe38b5dd6d666475e6dde95a346c57ca32733
Raghvendra-Riya <raghvendra.h.rajput@gmail.com>
2025-09-09 16:25:40 +0530

    Update SwitchToSpecificWindow.java

--- Selenium_Project/src/test/java/Com/Selenium/SwitchToSpecificWindow.java
+++ Selenium_Project/src/test/java/Com/Selenium/SwitchToSpecificWindow.java
@@ -19,11 +19,7 @@ public class SwitchToSpecificWindow {
         driver.get("https://opensource-demo.orangehrmlive.com/web/auth/login");
         
         Thread.sleep(4000);
-		Thread.sleep(4000);
-		Thread.sleep(4000);
-		Thread.sleep(4000);
-		Thread.sleep(4000);
-		Thread.sleep(4000);
+		
         driver.findElement(By.xpath("//a[contains(@href,'https://www.linkedin.com/company/orangehrm/mycompany/')]")).click();
         driver.findElement(By.xpath("(//a[contains(@href,'https://www.facebook.com/OrangeHRM/')])[1]")).click();
 //        driver.findElement(By.xpath("(//a[contains(@href,'https://www.facebook.com/OrangeHRM/')])[2]")).click();
@@ -31,14 +27,9 @@ public class SwitchToSpecificWindow {
         driver.findElement(By.xpath("//a[contains(@href,'https://www.youtube.com/c/OrangeHRMInc')]")).click();
 
         List<String> windlist=new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(windlist.get(4));
+        driver.switchTo().window(windlist.get(3));
         System.out.println("finally swithed to second tab:");
-        System.out.println("finally swithed to second tab:");
-        System.out.println("finally swithed to second tab:");
-        System.out.println("finally swithed to second tab:");
-
-        System.out.println("finally swithed");
-
+       
       
 
           
