2472d0138923720328268c18e4686992cd0690d5
Raghvendra-Riya <raghvendra.h.rajput@gmail.com>
2025-09-09 13:49:08 +0530

    final sop

--- Selenium_Project/src/test/java/Com/Selenium/SwitchToSpecificWindow.java
+++ Selenium_Project/src/test/java/Com/Selenium/SwitchToSpecificWindow.java
@@ -27,7 +27,7 @@ public class SwitchToSpecificWindow {
 
         List<String> windlist=new ArrayList<>(driver.getWindowHandles());
           driver.switchTo().window(windlist.get(3));
-          
+          System.out.println(driver.getTitle());
 	}
 }
 
