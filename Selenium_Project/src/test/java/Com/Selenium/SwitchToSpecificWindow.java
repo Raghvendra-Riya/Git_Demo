3974b4dcbe1d8a2661d9d2f0e7ea5f2667cb698d
Raghvendra-Riya <raghvendra.h.rajput@gmail.com>
2025-09-11 11:31:28 +0530

    getting conflict issue

--- Selenium_Project/src/test/java/Com/Selenium/SwitchToSpecificWindow.java
+++ Selenium_Project/src/test/java/Com/Selenium/SwitchToSpecificWindow.java
@@ -27,7 +27,7 @@ public class SwitchToSpecificWindow {
         driver.findElement(By.xpath("//a[contains(@href,'https://www.youtube.com/c/OrangeHRMInc')]")).click();
 
         List<String> windlist=new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(windlist.get(3));  
+        driver.switchTo().window(windlist.get(9));  
         System.out.println("finally swithed to second tab:");
        //HDJHEJWHFJKEHJKDHW
 		//rjhjkwehjkh
