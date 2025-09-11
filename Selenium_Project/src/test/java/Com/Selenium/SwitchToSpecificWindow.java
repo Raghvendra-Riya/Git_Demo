645589ef008c41b523632f699bb68b933dcb24ba
Raghvendra-Riya <raghvendra.h.rajput@gmail.com>
2025-09-11 17:33:56 +0530

    learned revert command

--- Selenium_Project/src/test/java/Com/Selenium/SwitchToSpecificWindow.java
+++ Selenium_Project/src/test/java/Com/Selenium/SwitchToSpecificWindow.java
@@ -29,7 +29,8 @@ public class SwitchToSpecificWindow {
         List<String> windlist=new ArrayList<>(driver.getWindowHandles());  
         driver.switchTo().window(windlist.get(3));  
         System.out.println("finally swithed to second tab:");
-     
+        System.out.println("learned revert command");
+ 
     
           
 	}
